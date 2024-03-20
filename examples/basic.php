7b023ab3f9411dcc954cddeda51a01f3c9be3b88
Matěj Humpál <finwe@users.noreply.github.com>
2024-03-20 17:51:21 +0100

    Fix examples and phpspec definition for Twig 3 (#16)

--- examples/basic.php
+++ examples/basic.php
@@ -2,12 +2,12 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-$loader = new Twig_Loader_Array(array(
+$loader = new Twig\Loader\ArrayLoader(array(
     'uniqid' => 'Hi, I am unique: {{ php_uniqid() }}.',
     'floor'  => 'And {{ php_floor(7.7) }} is floor of 7.7.',
 ));
 
-$twig = new Twig_Environment($loader);
+$twig = new Twig\Environment($loader);
 $twig->addExtension(new Umpirsky\Twig\Extension\PhpFunctionExtension());
 
 echo $twig->render('uniqid') . PHP_EOL;
